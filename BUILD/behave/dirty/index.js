162819d5dd3e8ac3bb0af397450323c96821817d
Marak Squires <marak.squires@gmail.com>
2010-05-12 03:37:43 -0400

    dirty behavior now working for three types of form inputs

--- BUILD/behave/dirty/index.js
+++ BUILD/behave/dirty/index.js
@@ -3,9 +3,23 @@ debug.log('dirty binded');
 debug.log(options.selector);
 
 
-// custom dirty behaviors for diffirents types of data binding
-var bindType = $(options.selector).attr('type');
+var is_dirty = function(){
+  debug.log('is dirty!', $(options.selector));
+  $(options.selector).data('is_dirty', true);
+};
 
+var is_clean = function(){
+  debug.log('is clean!', $(options.selector));
+  $(options.selector).data('is_dirty', false);  
+};
+
+// custom dirty behaviors for diffirents types of data binding
+var bindType = options.selector.nodeName;
+
+debug.log(bindType);
+if(bindType == 'INPUT'){
+  bindType = options.selector.type;
+}
 
 switch(bindType){
   
@@ -16,16 +30,44 @@ switch(bindType){
     $(options.selector).keyup(function(e){
       ng = $(this).val();
       if(og != ng){
-        debug.log('is dirty!')
+        is_dirty();
       }
       else{
-        debug.log('is clean!');
+        is_clean();
       }
     });
   break;
 
   case 'checkbox' :
     debug.log('checkbox');
+    $(options.selector).data('original_value', $(options.selector).attr('checked'));
+    var og = $(options.selector).data('original_value');
+    var ng = null;
+    $(options.selector).change(function(e){
+      ng = $(this).attr('checked');
+      if(og != ng){
+        is_dirty();
+      }
+      else{
+        is_clean();
+      }
+    });
+  break;
+
+  case 'SELECT' :
+    debug.log('select box');
+    $(options.selector).data('original_value', $(options.selector).val());
+    var og = $(options.selector).data('original_value');
+    var ng = null;
+    $(options.selector).change(function(e){
+      ng = $(this).val();
+      if(og != ng){
+        is_dirty();
+      }
+      else{
+        is_clean();
+      }
+    });
   break;
 
   
