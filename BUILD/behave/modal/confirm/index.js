c7c5615e83f9154444ebad21c73e5b4dfacd6458
Marak Squires <marak.squires@gmail.com>
2010-06-01 18:26:32 -0400

    working on new modal behaviors

---
+++ BUILD/behave/modal/confirm/index.js
@@ -0,0 +1,11 @@
+/* 
+
+ a confirm modal is a modal window that has two options, "OK" and "Cancel". 
+ if you need a custom modal you should be using modal-custom
+
+*/
+
+
+$(options.selector).hide();
+
+/* $(options.selector).dialog(); */
---
+++ BUILD/behave/modal/custom/index.js
@@ -0,0 +1,10 @@
+/* 
+
+ a custom modal is a modal window that can be completely customizable 
+
+*/
+
+
+$(options.selector).hide();
+
+/* $(options.selector).dialog(); */
