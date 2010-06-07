3feeabe30c2d36df3a9454558ae91678cf6223b0
Tom Jenkins <tom@thomasjenkins.net>
2010-06-07 16:17:20 -0400

    view change

--- BUILD/views/view.js
+++ BUILD/views/view.js
@@ -1,3 +1,16 @@
 return ["div",
-          {"data-behaviors":"nav-menu machine", "data-resource":"views", "class":"nav"}
-       ];
+       {"class": "container" },
+        ["div", 
+         {"class":"header span-24"},
+         
+         ["h1", "data-behaviors"]
+       ],
+       
+       ["div", 
+         {"class":"body span-24"},
+         ["div", 
+           {"data-behaviors":"nav-menu", "data-resource":"views", "class":"nav"}
+         ],
+         ["div", {"id":"navOutput", "data-behaviors":"machine"}, "this is the area to load stuff"]
+       ]
+   ];
