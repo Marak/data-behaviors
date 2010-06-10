16872738193bba071a901a3eb7b0ccca3488f4db
Tom Jenkins <tom@thomasjenkins.net>
2010-06-10 17:16:31 -0400

    Removing some container classes that are redundant

--- BUILD/views/behaviors/view.js
+++ BUILD/views/behaviors/view.js
@@ -1,2 +1,2 @@
 [["div", {"data-behaviors":"nav-menu", "data-resource":"['charts', 'forms', 'navigation']", "class":"menu"}], 
-         ["div", {"id":"subOutput", "data-behaviors":"machine"}, "asdasd"]]
+         ["div", {"id":"subOutput", "data-behaviors":"machine"}]]
