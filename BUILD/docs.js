7cfe6134df69e8deccca1906318343968f333c43
Marak Squires <marak.squires@gmail.com>
2010-06-21 16:53:25 -0400

    updating docs

--- BUILD/docs.js
+++ BUILD/docs.js
@@ -1,1 +1,13 @@
-<h1>data-behaviors</h1> 
+<h1>data-behaviors</h1>
+
+data-behaviors is an unobstrusive open-source behaviors library built on top of jQuery and the jQuery UI and uses all the best jQuery plugins
+data-behaviors utilizes HTML5s new <a href = "http://ejohn.org/blog/html-5-data-attributes/>""data- attributes"</a>, while still maintaining backwards compatibility with IE6
+
+data-behaviors was built from the ground up to help facilitate in the creation of quick, relatively rich, javascript user interfaces.
+
+key goals :
+
+make the library and API as simple as possible. (see usage docs)
+fulfill the majority (we aim for 80%) of the needs of a rich UI out of the box, with no configuration while still allowing developers to customize the additional 20% with no artificial roadblocks or poorly thought out library architecture 
+eliminating the need to piece together various jQuery plugins for your project over and over again
+be so unobstrusive that no one on your dev team will have any excuse to not love the library!
